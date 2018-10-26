84a889c5dd23c15c996887ea1ded5c4e01d2a5ba
Kurtis Angell <KurtisBlow@MacBook-Pro.local>
2018-10-26 15:55:14 +0100

    second

--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const port = process.env.PORT || 9090;
+const port = process.env || 9090;
 
 app.listen(port, () => {
     console.log('server listenning on port 9090');
