3cc4bbf10750ba6fddc5564fac392768a5e6d95f
Kurtis Angell <KurtisBlow@MacBook-Pro.local>
2018-11-15 12:03:17 +0000

    potential comment fix

--- controllers/commentsControllers.js
+++ controllers/commentsControllers.js
@@ -16,14 +16,15 @@ const deleteComment = (req, res, next) => {
 
 const voteQuery = (req, res, next) => {
     const query = req.query.vote
-    const invalidId = req.params.comment_id
-    Comment.findOneAndUpdate(invalidId, query === 'up' ? { $inc: { 'votes': 1 } } : query === 'down' ? { $inc: { 'votes': - 1 } } : { $inc: { 'votes': 0 } }, { new: true })
+    const id = req.params.comment_id
+    Comment.findByIdAndUpdate(id, query === 'up' ? { $inc: { 'votes': 1 } } : query === 'down' ? { $inc: { 'votes': - 1 } } : { $inc: { 'votes': 0 } }, { new: true })
         .then(article => {
             //because the 'true' returns a new data entry 
-            // if (JSON.stringify(article._id) !== JSON.stringify(invalidId)) {
-            //     return Promise.reject({ status: 404, msg: 'valid ID does not exist' })
-            // } else {
-            res.send(article)
+            if (JSON.stringify(article._id) !== JSON.stringify(invalidId)) {
+                return Promise.reject({ status: 404, msg: 'valid ID does not exist' })
+            } else {
+                res.send(article)
+            }
         })
         .catch(next)
 }
